7d0812655c9392922c3fbca08c145ed4d4d346cf
Nihir <nihirvedd@gmail.com>
2025-03-18 11:47:28 +0000

    runs in a loop

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from app.db_utils import DBUtils
 from app.data_processor import DataProcessor
 from app.train_model import TrainModel
-
+import time
+from datetime import datetime
 
 class Orchestrator:
     def __init__(self):
@@ -10,12 +11,19 @@ class Orchestrator:
         self.model_trainer = TrainModel()
 
     def run(self):
+        while True:
+            if datetime.now(tz=timezone.utc).minute == 00:
+                self.process_data()
+                time.sleep(60) # wait a minute to ensure we don't run the same tick twice
+        
+    def process_data(self):
         docs = self.db_client.find_documents_and_groupby({"metaGeomMScore1": {"$exists": True}}, "clientId")
         docs_df = self.data_processor.build_ads_dataframe(docs)
         processed_for_plots = self.data_processor.process_data_for_plots(docs_df)
         self.db_client.serialise_eda(processed_for_plots)
         model_trained_data = self.model_trainer.run(docs_df)
         self.db_client.serialise_model(model_trained_data)
+        
         
 if __name__ == "__main__":
     orchestrator = Orchestrator()
